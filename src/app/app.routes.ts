a76e0aaf525cc65792557937010a7e8db4c1fb38
D4ND14Z <dandiazfo@gmail.com>
2026-01-11 01:57:45 -0500

    (3) - MOdificaciones en Routes. Creacion de componentes MenuBar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { ContactPage } from './pages/contact-page/contact-page';
+import { HomePage } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePage },
+    {path: 'login', component: LoginPage },
+    {path: 'about', component: AboutPage },
+    {path: 'contact', component: ContactPage },
+    {path: '**', redirectTo: '' }
+];
---
+++ src/app/components/menubar-component/menubar-component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+  selector: 'menubar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './menubar-component.html',
+  styleUrls: ['./menubar-component.css'],
+})
+export class MenubarComponent {
+
+  constructor(private router: Router) {}
+
+  public goToLogin = () => {
+    this.router.navigate(['/login']);
+  }
+
+}
