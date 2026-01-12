f7ad6b69ab120e158f0e0da4a814459587dff9c1
d4nd14z <dandiazfo@gmail.com>
2026-01-11 19:41:48 -0500

    (6) - Creacion de la page 'DragonBall' ajuste de css en la pagina y ajuste de bordes. (Solo para DragonBall)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginPage } from './pages/login-page/login-page';
 import { AboutPage } from './pages/about-page/about-page';
 import { ContactPage } from './pages/contact-page/contact-page';
 import { HomePage } from './pages/home-page/home-page';
+import { DragonballPage } from './pages/dragonball-page/dragonball-page';
 
 export const routes: Routes = [
     {path: '', component: HomePage },
     {path: 'login', component: LoginPage },
     {path: 'about', component: AboutPage },
     {path: 'contact', component: ContactPage },
+    {path: 'dragonball', component: DragonballPage },
     {path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/dragonball-page/dragonball-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MenubarComponent } from '../../components/shared/menubar-component/menubar-component';
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [ MenubarComponent ],
+  templateUrl: './dragonball-page.html',
+  styleUrl: './dragonball-page.css',
+})
+export class DragonballPage {
+
+}
